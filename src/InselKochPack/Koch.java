2459af78448b77c8c4ffa2a6e5223d8dea14bb88
DukeOfWar <93122756+DukeOfWar@users.noreply.github.com>
2021-10-31 20:23:45 +0100

    Java Projekt
    InselKoch

---
+++ src/InselKochPack/Koch.java
@@ -0,0 +1,10 @@
+package InselKochPack;
+
+public class Koch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Viel Spaﬂ beim Kochen");
+	}
+
+}
